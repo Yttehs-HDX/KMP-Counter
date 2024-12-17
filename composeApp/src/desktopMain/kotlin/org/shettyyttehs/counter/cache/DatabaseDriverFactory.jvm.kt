7f93b0c2369226f3c934da227f689307e8171e0c
Yttehs <shetty.yttehs@outlook.com>
2024-12-17 16:54:47 +0800

    fix: [Desktop] set path for JDBC

---
+++ composeApp/src/desktopMain/kotlin/org/shettyyttehs/counter/cache/DatabaseDriverFactory.jvm.kt
@@ -0,0 +1,32 @@
+package org.shettyyttehs.counter.cache
+
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import java.io.File
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual class DatabaseDriverFactory {
+    actual fun createDriver(): SqlDriver {
+        val dbPath = getDatabasePath()
+        val driver = JdbcSqliteDriver(
+            url = "jdbc:sqlite:$dbPath"
+        )
+
+        // Create the database if it doesn't exist
+        val dbFile = File(dbPath)
+        if (!dbFile.exists()) {
+            AppDatabase.Schema.create(driver)
+        }
+
+        return driver
+    }
+}
+
+actual val databaseFactory by lazy {
+    DatabaseDriverFactory()
+}
+
+fun getDatabasePath(): String {
+    // Test environment
+    return "counter.db"
+}
