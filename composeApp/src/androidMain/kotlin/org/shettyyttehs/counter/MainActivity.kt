4d5551c610a7b2781cbc0c4c9b99f7388e7e373a
Yttehs <shetty.yttehs@outlook.com>
2024-12-17 17:33:04 +0800

    feat: [android] implement statusbar/navigationbar color

--- composeApp/src/androidMain/kotlin/org/shettyyttehs/counter/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/shettyyttehs/counter/MainActivity.kt
@@ -3,15 +3,40 @@ package org.shettyyttehs.counter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
+
         setContent {
-            App()
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            listOf(
+                                MaterialTheme.colorScheme.background,
+                                NavigationBarDefaults.containerColor,
+                            )
+                        )
+                    )
+                    .safeDrawingPadding(),
+            ) {
+                App()
+            }
         }
     }
 }
